70b8b0419a6c1507da2e5dad030f356ac7dba365
pdxor <admin@mirandacreative.com>
2018-04-10 15:37:03 -0400

    finished dynamic headerinclude which is mostly on hero.php

--- inc/hero.php
+++ inc/hero.php
@@ -1,11 +1,15 @@
-<header class="hero">
+<!-- This is a ternary operator that chooses the default hero if an overide wasnt chosen on a ind page -->
+<?php 
+$herourl = get_field('hero') ? get_field('hero') : get_field('default_hero', 'option'); 
+?>
+<header class="hero" style="background: url(<?= $herourl ?>) no-repeat;    background-size: cover;">
   <div class="hero-inner">
    <div class="search">
       <span class="fa fa-search"></span>
       <input placeholder="Search...">
     </div>
-    <div class="mx-auto logo-hold">
-      <img src="<?php echo get_stylesheet_directory_uri();?>/img/marchfarm-white.svg">
+    <div class="mx-auto logo-hold">   	
+      <img src="<?php the_field('logo', 'option'); ?>">
     </div>
   </div>
 </header>
