3495c328dabf9ec31623b9b4eab630f07c6b1b6d
pdxor <admin@mirandacreative.com>
2018-04-18 16:29:48 -0400

    Added search functionality. Search still needs some style assistance

--- inc/hero.php
+++ inc/hero.php
@@ -1,14 +1,19 @@
 <!-- This is a ternary operator that chooses the default hero if an overide wasnt chosen on a ind page -->
-<?php 
-$herourl = get_field('hero') ? get_field('hero') : get_field('default_hero', 'option'); 
+<?php
+$herourl = get_field('hero') ? get_field('hero') : get_field('default_hero', 'option');
 ?>
 <header class="hero" style="background: url(<?= $herourl ?>) no-repeat;    background-size: cover;">
   <div class="hero-inner">
    <div class="search">
-      <span class="fa fa-search"></span>
-      <input placeholder="Search...">
+     <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+      <input id="wpsearch" type="search" class="search-field"
+      placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
+      value="<?php echo get_search_query() ?>" name="s"
+      title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+    <button type="submit" class="search-submit"><i class="fa fa-search"></i></button>
+    </form>
     </div>
-    <div class="mx-auto logo-hold">   	
+    <div class="mx-auto logo-hold">
       <img src="<?php the_field('logo', 'option'); ?>">
     </div>
   </div>
