0a6dd77a7a39cd02708f4ece26e2091c6621f579
pdxor <admin@mirandacreative.com>
2018-04-19 12:06:19 -0400

    fixed fallback index.php template

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
-<?php get_header(); ?>
-        <?php include "inc/parts/blurb.php" ?>
-        <?php include "inc/parts/highlights.php" ?>
-        <?php include "inc/parts/gallerysquare.php" ?>
-        <?php include "inc/parts/cta.php" ?>
-        <?php include "inc/parts/events.php" ?>
-        <?php include "inc/parts/calloutimgleft.php" ?>
-        <?php include "inc/parts/videotext.php" ?> <!-- Needs Javascript input -->
-        <?php include "inc/parts/map.php" ?>
-    <?php include "inc/megafooter.php" ?>
-    <?php include "inc/footer.php" ?>
+<?php
+get_header(); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <?php the_content(); ?>
+          <?php edit_post_link(); ?>
+      <?php endwhile; else: ?>
+          <?php _e( 'Sorry, no pages matched your criteria.', 'textdomain' ); ?>
+      <?php endif; ?>
+      <?php include "inc/flex.php" ?>
+      <?php include "inc/parts/map.php" ?>
+      <?php include "inc/megafooter.php" ?>
+      <?php include "inc/footer.php" ?>
   </body>
   <?php wp_footer(); ?>
 </html>
