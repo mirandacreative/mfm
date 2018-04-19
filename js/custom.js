9efc5a5677c54f90365e5a41946a91786a13fe4a
pdxor <admin@mirandacreative.com>
2018-04-19 11:51:40 -0400

    Added youtube functyionality for video text module

--- js/custom.js
+++ js/custom.js
@@ -7,13 +7,30 @@ jQuery(function($) {
       $(".dropdown-toggle").removeAttr('data-toggle dropdown');
     }
   });
+  // Videotext module js
+  $('#play-video').on('click', function(ev) {
+    $('#play-video img').css("display","none");
+    $("#video")[0].src += "&autoplay=1";
+    ev.preventDefault();
+  });
+  // Calculate height and width of responsive video thumb in order to get a responsive YT iframe
+  $vidHeight = $("#measure").height();
+  $vidWidth = $("#measure").width();
+  $("#video").attr('width', $vidWidth);
+  $("#video").attr('height', $vidHeight);
+
+  function hideVideo(){
+    $('#play-video img').css("display","none");
+    $("#video")[0].src += "&autoplay=1";
+    ev.preventDefault();
+  }
 
 });
 
 
 var sizer = $( window ).width();
-	
+
 if(sizer > 768){
 
-	$("ul.sub-menu").css("display","block"); 
+	$("ul.sub-menu").css("display","block");
 }
